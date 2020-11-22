5fffbb03ccf1e5ebf89104b90e5eeacff7c5ad0f
Harish S.G <harishsg99@gmail.com>
2020-11-22 09:07:54 +0530

    Create ScoopMLcli.py

---
+++ ScoopML/ScoopMLcli.py
@@ -0,0 +1,18 @@
+import click
+
+
+@click.group()
+def apis():
+    """A CLI for managing ScoopML Cloud AI"""
+    
+def categories():
+    """List all categories."""
+    response = requests.get(url=f'{BASE_URL}/categories')
+    if response.status_code is 200:
+        print('\n'.join(response.json()))
+    else:
+        print(f'Could not get the categories: {response.text}')
+
+
+if __name__ == '__main__':
+    apis(prog_name='apis')
